5f6a70730f6c2ed43c5b43ccda24f1d811cd7324
sanklss <sanklsswt2@yandex.ru>
2026-03-03 17:12:04 +0300

    Добавление и реализация класса User

---
+++ Homework3/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework3
+{
+    public abstract class User
+    {
+        private string _role;
+
+        public User(string role)
+        {
+            _role = role;
+        }
+
+        public string GetRole()
+        {
+            return _role;
+        }
+    }
+}
