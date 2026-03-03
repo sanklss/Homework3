74d0a06ba42db8544e32301f0a48d6b237225025
sanklss <sanklsswt2@yandex.ru>
2026-03-03 17:54:19 +0300

    Добавление метода для подсчета количества пользователей в UserFactory

--- Homework3/UserFactory.cs
+++ Homework3/UserFactory.cs
@@ -1,7 +1,37 @@
-﻿namespace Homework3
-{
-    public class UserFactory
-    {
+﻿using Homework3.Users;
 
+namespace Homework3
+{
+    public static class UserFactory
+    {
+        private static int _clientCount;
+        private static int _adminCount;
+        private static int _managerCount;
+
+        public static User CreateUser(string role)
+        {
+            switch (role)
+            {
+                case "Клиент":
+                    _clientCount++;
+                    return new Client();
+
+                case "Администратор":
+                    _adminCount++;
+                    return new Admin();
+
+                case "Менеджер":
+                    _managerCount++;
+                    return new Manager();
+
+                default:
+                    return null;
+            }
+        }
+
+        public static string GetCountUsers()
+        {
+            return $"Клиент - {_clientCount}, Администратор - {_adminCount}, Менеджер - {_managerCount}.";
+        }
     }
 }
